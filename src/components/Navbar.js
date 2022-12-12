e0f4b091d98efd57438e89a7d28ef865746a61cc
sammyart <27844334+sammyart@users.noreply.github.com>
2022-12-12 16:19:02 -0700

    change blog to library in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -44,8 +44,8 @@ const Navbar = () => {
             </Link>
             </li>
             <li className="navbar-item" style={{padding: "0px"}}>
-            <Link className="navbar-item" to="/blog">
-              Blog
+            <Link className="navbar-item" to="/library">
+              Library
             </Link>
             </li>
             <li className="navbar-item" style={{padding: "0px"}}>
